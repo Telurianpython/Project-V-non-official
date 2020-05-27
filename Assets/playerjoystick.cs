d78bbe2289c4b775863d99bdbd3d43686d8d56c0
Tellurian <Telurianpython@gmail.com>
2020-05-27 17:31:16 -0500

    not working yet

--- Assets/playerjoystick.cs
+++ Assets/playerjoystick.cs
@@ -10,21 +10,25 @@ public class playerjoystick : MonoBehaviour
     {
        float hoz = moveJoystick.Horizontal;
        float ver =  moveJoystick.Vertical;
-       Vector3 direction = new Vector3(hoz, 0, ver).normalized;
+
+       Vecter2 convertedXY= ConvertWithCamera(Camera.main.transform.position, hoz, ver);
+
+       Vector3 direction = new Vector3(convertedXY.x, 0, convertedXY.y).normalized;
        transform.Translate(direction * 0.5f, Space.World);
     }
 
     private Vector2 ConvertWithCamera(Vector3 cameraPos, float hor, float ver)
     {
-        Vector2 joyDirection = new Vector2(hor, vector).normalized;
+        Vector2 joyDirection = new Vector2(hor, ver).normalized;
         Vector2 camera2DPos = new Vector2(camera2DPos.x, camera2DPos.z);
         Vector2 playerPos = new Vector2(transform.position.x, transform.position.z);
         Vector2 cameraToPlayerDirection = (Vector2.zero - camera2DPos).normalized;
-        float angle = Vector2.SignedAngle(cameraToPlayerDirection, new Vecto2(0, 1));
+        float angle = Vector2.SignedAngle(cameraToPlayerDirection, new Vector2(0, 1));
+        Vecter2 finalDirection = RotateVector(joyDirection, -angle);
         return finalDirection;
     }
 
-    public Vector2 RotateVector(Vector2 vector, float angle)
+    public Vector2 RotateVector(Vector2 v, float angle)
     {
         float radian = angle * Mathf.Deg2Rad;
         float _x = v.x * Mathf.Cos(radian) - v.y * Mathf.Sin(radian);
