057d84a2c203f8817df4fef6b3a37908100e4532
Tellurian <Telurianpython@gmail.com>
2020-05-26 21:45:56 -0500

    update 2

--- Assets/playerjoystick.cs
+++ Assets/playerjoystick.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class playerjoystick : MonoBehaviour
 {
-    public FixedJoyStick moveJoystick;
-    
+    public FixedJoystick moveJoystick;
     // Update is called once per frame
     void Update()
     {
        float hoz = moveJoystick.Horizontal;
        float ver =  moveJoystick.Vertical;
-       Vector3 direction = new Vector3(hoz, 0, ver).noralized;
-       transform.Translate(direction * 0.02f, space.world);
+       Vector3 direction = new Vector3(hoz, 0, ver).normalized;
+       transform.Translate(direction * 0.5f, Space.World);
     }
 }
+ private Vector2 RotateVector(Vector2 vector, float angle)
+ {
+     Vector2 joyDirection = new Vector2(hor, vector).normalized;
+     Vector2 camera2DPos = new Vector2(camera2DPos.x, camera2DPos.z);
+     Vector2 playerPos = new Vector2(transform.position.x, transform.position.z);
+     Vector2 cameraToPlayerDirection = (Vector2.zero - camera2DPos).normalized);
+
+ }
