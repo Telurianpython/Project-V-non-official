5ed7f33aedae117857b33344021dfef0d7750605
Tellurian <Telurianpython@gmail.com>
2020-05-26 20:59:45 -0500

    update script

--- Assets/playerjoystick.cs
+++ Assets/playerjoystick.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class playerjoystick : MonoBehaviour
 {
-    // Start is called before the first frame updat
-    void Start()
-    {
-        
-    }
-
+    public FixedJoyStick moveJoystick;
+    
     // Update is called once per frame
     void Update()
     {
-        
+       float hoz = moveJoystick.Horizontal;
+       float ver =  moveJoystick.Vertical;
+       Vector3 direction = new Vector3(hoz, 0, ver).noralized;
+       transform.Translate(direction * 0.02f, space.world);
     }
 }
