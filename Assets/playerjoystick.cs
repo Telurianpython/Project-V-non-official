0206fc9bc1f1171359bbb4b7221c9de6071885a4
Tellurian <Telurianpython@gmail.com>
2020-05-27 19:23:26 -0500

    everything working(this is a prototype)

--- Assets/playerjoystick.cs
+++ Assets/playerjoystick.cs
@@ -5,26 +5,41 @@ using UnityEngine;
 public class playerjoystick : MonoBehaviour
 {
     public FixedJoystick moveJoystick;
+    public FixedJoystick lookJoystick;
     // Update is called once per frame
     void Update()
     {
+       UpdateMoveJoystick();
+       UpdatelookJoystick();
+    }
+
+    void UpdateMoveJoystick()
+    {
        float hoz = moveJoystick.Horizontal;
        float ver =  moveJoystick.Vertical;
-
-       Vecter2 convertedXY= ConvertWithCamera(Camera.main.transform.position, hoz, ver);
-
+       Vector2 convertedXY= ConvertWithCamera(Camera.main.transform.position, hoz, ver);
        Vector3 direction = new Vector3(convertedXY.x, 0, convertedXY.y).normalized;
        transform.Translate(direction * 0.5f, Space.World);
+    }
+
+        void UpdatelookJoystick()
+    {
+       float hoz = lookJoystick.Horizontal;
+       float ver =  lookJoystick.Vertical;
+       Vector2 convertedXY= ConvertWithCamera(Camera.main.transform.position, hoz, ver);
+       Vector3 direction = new Vector3(convertedXY.x, 0, convertedXY.y).normalized;
+       Vector3 lookAtPosition = transform.position + direction;
+       transform.LookAt(lookAtPosition);
     }
 
     private Vector2 ConvertWithCamera(Vector3 cameraPos, float hor, float ver)
     {
         Vector2 joyDirection = new Vector2(hor, ver).normalized;
-        Vector2 camera2DPos = new Vector2(camera2DPos.x, camera2DPos.z);
+        Vector2 camera2DPos = new Vector2(cameraPos.x, cameraPos.z);
         Vector2 playerPos = new Vector2(transform.position.x, transform.position.z);
         Vector2 cameraToPlayerDirection = (Vector2.zero - camera2DPos).normalized;
         float angle = Vector2.SignedAngle(cameraToPlayerDirection, new Vector2(0, 1));
-        Vecter2 finalDirection = RotateVector(joyDirection, -angle);
+        Vector2 finalDirection = RotateVector(joyDirection, -angle);
         return finalDirection;
     }
 
